c336c3e4b87a69792d2ae150848498c2fd3cb673
andy-anu <8765anurag@gmail.com>
2020-08-05 23:30:32 +0530

    Updates

---
+++ precache-manifest.c6913e81c0da1bc0cc78c50a3c7b5744.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c70fbb769fd45cfeee67e9d7cfaebc0",
+    "url": "/insta_clone/index.html"
+  },
+  {
+    "revision": "5fc040d2a3317f1c39a3",
+    "url": "/insta_clone/static/css/main.d90151d9.chunk.css"
+  },
+  {
+    "revision": "dd099746e0d90a11989e",
+    "url": "/insta_clone/static/js/2.c4a3b69f.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/insta_clone/static/js/2.c4a3b69f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fc040d2a3317f1c39a3",
+    "url": "/insta_clone/static/js/main.746e92c3.chunk.js"
+  },
+  {
+    "revision": "3f6c4437bf8c4b14465e",
+    "url": "/insta_clone/static/js/runtime-main.2e6140db.js"
+  }
+]);
